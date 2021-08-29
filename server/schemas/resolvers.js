4c42dd87e2691d8c9827a70cf944c78831ab573e
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-29 12:04:17 -0400

    typeDefs and resolvers in place

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,72 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('savedBooks')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const userUpdate = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: args.input } },
+                    { new: true }
+                ).populate('savedBooks');
+
+                return userUpdate;
+            }
+
+            throw new AuthenticationError('Need to be logged in to add a book to your list');
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const userUpdate = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                ).populate('savedBooks');
+                
+                return userUpdate;
+            }
+
+            throw new AuthenticationError('Need to be logged in to add a book to your list');
+        } 
+    }
+};
+
+module.exports = resolvers;
