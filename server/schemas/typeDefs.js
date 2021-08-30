ea323415bc33619a24fbea11731874a920fb34a8
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-30 07:36:16 -0400

    done with forms

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,7 +42,7 @@ const typeDefs = gql`
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
         saveBook(book: bookToSave): User
-        removeBook(boodId: String!): User
+        removeBook(bookId: String!): User
     }
 `;
 
