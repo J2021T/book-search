e753eb03d82444f76c3527b0bf2905c86995fe67
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-30 12:33:35 -0400

    files in place

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser, {error}] = useMutation(ADD_USER);
+  const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -34,7 +34,8 @@ const SignupForm = () => {
         variables: { ...userFormData }
       });
 
-      Auth.login(data.createUser.token)
+      console.log(data.addUser.token);
+      Auth.login(data.addUser.token);
 
     } catch (err) {
       console.error(err);
